c9df71134051ec08d139bc1984ebbde390218968
paseflo76 <paseflo34@gmail.com>
2025-06-25 18:36:36 +0200

    cambio en componente button

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -2,12 +2,13 @@ import './button.css'
 
 //! funcion componente button
 
-export const Button = (texto, importance, size, type = 'button') => {
+export const Button = (nodoPadre, texto, importance, size, type = 'button') => {
   const button = document.createElement('button')
-  button.type = type
   button.classList = 'main-button'
   button.classList.add(importance)
   button.classList.add(size)
   button.textContent = texto
+
+  if (nodoPadre) nodoPadre.append(button)
   return button
 }
