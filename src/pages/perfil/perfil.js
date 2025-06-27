8c3a421584142b72ed5c85e75e7a3997646a183d
paseflo76 <paseflo34@gmail.com>
2025-06-27 11:09:42 +0200

    añadir css a perfil y correccion en perfil.js

--- src/pages/perfil/perfil.js
+++ src/pages/perfil/perfil.js
@@ -1,26 +1,29 @@
+import './perfil.css'
 import { apiCatch } from '../../utils/fetch/fech'
 import { navigate } from '../../main'
-
-import './perfil.css'
 import { Button } from '../../components/button/button'
 
 export const Perfil = () => {
   const main = document.querySelector('main')
   main.innerHTML = ''
 
-  const container = document.createElement('div')
+  const container = document.createElement('section')
+  container.className = 'perfil-container'
+
   const title = document.createElement('h2')
   title.textContent = 'Perfil de usuario'
+  title.className = 'perfil-title'
 
-  const btnEliminar = Button(container, 'Eliminar cuenta', 'secondary', 's')
+  const btnEliminar = Button(null, 'Eliminar cuenta', 'secondary', 's')
+  btnEliminar.classList.add('perfil-btn')
   btnEliminar.addEventListener('click', async () => {
     const token = localStorage.getItem('token')
-    const userId = localStorage.getItem('userId') // asegúrate de guardar esto al iniciar sesión
+    const userId = localStorage.getItem('userId')
     await apiCatch(`/api/v2/users/${userId}`, 'DELETE', null, token)
     localStorage.removeItem('token')
     navigate('home')
   })
 
-  container.appendChild(title)
+  container.append(title, btnEliminar)
   main.appendChild(container)
 }
