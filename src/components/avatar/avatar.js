c9b20e1c505ed5e7ee465564aafcf419ca57bdde
paseflo76 <paseflo34@gmail.com>
2025-08-19 11:15:57 +0200

    poner avatar por defecto

--- src/components/avatar/avatar.js
+++ src/components/avatar/avatar.js
@@ -1,7 +1,7 @@
 export const Avatar = ({ src, alt }) => {
   const img = document.createElement('img')
   img.className = 'perfil-avatar'
-  img.src = src || '/default-avatar.png'
+  img.src = src || '/assets/avatar.png'
   img.alt = alt || 'Avatar de usuario'
   return img
 }
