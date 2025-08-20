ce66008aa393b15844919ed0fc6ae79ee9fa19ea
paseflo76 <paseflo34@gmail.com>
2025-08-20 09:43:14 +0200

    loader en avatar

--- src/components/avatar/avatar.js
+++ src/components/avatar/avatar.js
@@ -1,7 +1,34 @@
-export const Avatar = ({ src, alt }) => {
+/* export const Avatar = ({ src, alt }) => {
   const img = document.createElement('img')
   img.className = 'perfil-avatar'
   img.src = src || '/assets/avatar.png'
   img.alt = alt || 'Avatar de usuario'
   return img
+}
+ */
+export const Avatar = ({ src, alt }) => {
+  const wrapper = document.createElement('div')
+  wrapper.className = 'avatar-wrapper'
+
+  const loader = document.createElement('div')
+  loader.className = 'avatar-loader'
+  loader.innerHTML = '<div class="spinner"></div>'
+  wrapper.appendChild(loader)
+
+  const img = document.createElement('img')
+  img.className = 'perfil-avatar'
+  img.src = src || '/assets/avatar.png'
+  img.alt = alt || 'Avatar de usuario'
+  img.style.opacity = '0'
+  img.style.cursor = 'not-allowed'
+  img.draggable = false
+
+  img.onload = () => {
+    loader.remove()
+    img.style.opacity = '1'
+    img.style.cursor = 'default'
+  }
+
+  wrapper.appendChild(img)
+  return wrapper
 }
