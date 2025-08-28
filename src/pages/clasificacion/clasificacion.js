46971205a852538a7be58ba92bedc8b9633c34d0
paseflo76 <paseflo34@gmail.com>
2025-08-28 17:39:10 +0200

    correcion en page clasifi

--- src/pages/clasificacion/clasificacion.js
+++ src/pages/clasificacion/clasificacion.js
@@ -1,10 +1,8 @@
-// pages/clasificacion/clasificacion.js
 import { renderClasificacion } from '../../utils/clasificacion.js/clasificacion'
 import './clasificacion.css'
 
 export const Clasificacion = async () => {
   const main = document.querySelector('main')
   main.innerHTML = `<div id="clasificacion"></div>`
-  const container = document.querySelector('main div')
-  await renderClasificacion(container)
+  await renderClasificacion('clasificacion') // pasar id como string
 }
