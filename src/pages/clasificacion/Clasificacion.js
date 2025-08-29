f2d8ac7f0c800ba54cd2193f2ffff67a5e4b0bd5
paseflo76 <paseflo34@gmail.com>
2025-08-29 19:52:57 +0200

    correciones

--- src/pages/clasificacion/Clasificacion.js
+++ src/pages/clasificacion/Clasificacion.js
@@ -1,24 +1,19 @@
-import { renderClasificacion } from '../../utils/clasificacion.js/clasifi'
-import { renderJornadas } from '../../utils/jornadas/jornadas'
+import { renderClasificacion } from '../../utils/clasificacion/Clasificacion.js'
+import { renderJornadas } from '../../utils/jornadas/jornadas.js'
 import './clasificacion.css'
 
 export const Clasificacion = async () => {
   const main = document.querySelector('main')
   main.innerHTML = ''
 
-  // Contenedor para clasificación
   const divClasificacion = document.createElement('div')
   divClasificacion.id = 'clasificacion'
   main.appendChild(divClasificacion)
 
-  // Contenedor para jornadas (opcional, se puede quitar si solo quieres tabla)
   const divJornadas = document.createElement('div')
   divJornadas.id = 'jornadas-clasificacion'
   main.appendChild(divJornadas)
 
-  // Renderizar clasificación
   await renderClasificacion(divClasificacion)
-
-  // Renderizar jornadas debajo de la clasificación
-  await renderJornadas(divJornadas, divClasificacion) // pasamos divClasificacion para actualizar
+  await renderJornadas(divJornadas, divClasificacion)
 }
