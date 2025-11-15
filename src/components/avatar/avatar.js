a4dec50719657179fab3ac19bb2819e2bc6a692e
paseflo76 <paseflo34@gmail.com>
2025-11-15 20:44:34 +0100

    css para que nochoche con el de clasifi

--- src/components/avatar/avatar.js
+++ src/components/avatar/avatar.js
@@ -1,11 +1,4 @@
-/* export const Avatar = ({ src, alt }) => {
-  const img = document.createElement('img')
-  img.className = 'perfil-avatar'
-  img.src = src || '/assets/avatar.png'
-  img.alt = alt || 'Avatar de usuario'
-  return img
-}
- */
+
 
 import './avatar.css'
 export const Avatar = ({ src, alt }) => {
