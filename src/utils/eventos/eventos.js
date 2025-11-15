e04db76e81d79d5f36131f1c0e657ca5cc1ffed3
paseflo76 <paseflo34@gmail.com>
2025-11-15 17:56:35 +0100

    corecion en eventos

--- src/utils/eventos/eventos.js
+++ src/utils/eventos/eventos.js
@@ -3,7 +3,7 @@ import { apiCatch } from '../fetch/fech'
 export const getEventos = async () => {
   try {
     // Solo '/eventos', API_BASE ya incluye /api/v2
-    return await apiCatch('/eventos')
+    return await apiCatch('/api/v2/eventos')
   } catch (error) {
     console.error('Error al obtener eventos:', error)
     return []
